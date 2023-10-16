15dcd24193037e77d9c37aacc9a0f02dfb57f18a
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2023-10-16 17:40:39 +0200

    cosmetics

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ fn main() {
         Data::new(1, 1, 1),
     ];
     let mut nn_or = NN::new();
-    nn_or.train(data_or, 0.1, 1000000);
+    nn_or.train(data_or, 0.5, 1000000);
     nn_or.predict(0, 1);
     nn_or.predict(1, 0);
     nn_or.predict(0, 0);
     nn_or.predict(1, 1);
+    println!("------------------------------");
 
     let data_and: Vec<Data> = vec![
         Data::new(0, 0, 0),
--- src/neural.rs
+++ src/neural.rs
@@ -80,7 +80,6 @@ struct Neurons {
     x1: Neuron,
     h0: Neuron,
     h1: Neuron,
-    h2: Neuron,
     o0: Neuron,
 }
 
@@ -92,7 +91,6 @@ impl Neurons {
             x1: Neuron::new(),
             h0: Neuron::new(),
             h1: Neuron::new(),
-            h2: Neuron::new(),
             o0: Neuron::new(),
         }
     }
@@ -118,52 +116,49 @@ impl NN {
     pub fn train(&mut self, data: Vec<Data>, learn_rate: f32, epochs: i32) {
         let start = std::time::Instant::now();
         for _ in 0..epochs {
-            for (data_slice) in data.iter() {
-                let sum_h1 = self.conns.w0.weight * data_slice.x
+            for data_slice in data.iter() {
+                let sum_h0 = self.conns.w0.weight * data_slice.x
                     + self.conns.w1.weight * data_slice.y
+                    + self.neurons.h1.bias;
+                let h0 = sigmoid(sum_h0);
+                let h0_der = derivsigmoid(sum_h0);
+
+                let sum_h1 = self.conns.w2.weight * data_slice.x
+                    + self.conns.w3.weight * data_slice.y
                     + self.neurons.h1.bias;
                 let h1 = sigmoid(sum_h1);
                 let h1_der = derivsigmoid(sum_h1);
 
-                let sum_h2 = self.conns.w2.weight * data_slice.x
-                    + self.conns.w3.weight * data_slice.y
-                    + self.neurons.h2.bias;
-                let h2 = sigmoid(sum_h2);
-                let h2_der = derivsigmoid(sum_h2);
-
                 let sum_o0 =
-                    self.conns.w4.weight * h1 + self.conns.w5.weight * h2 + self.neurons.o0.bias;
+                    self.conns.w4.weight * h0 + self.conns.w5.weight * h1 + self.neurons.o0.bias;
                 let o0 = sigmoid(sum_o0);
                 let o0_der = derivsigmoid(sum_o0);
 
                 let d_mse = -2.0 * (data_slice.out - o0);
 
-                let d_w5 = h1 * o0_der;
-                let d_w6 = h2 * o0_der;
-                let d_b3 = o0_der;
+                let d_w5 = h0 * o0_der;
+                let d_w6 = h1 * o0_der;
 
-                let d_h1 = self.conns.w4.weight * o0_der;
-                let d_h2 = self.conns.w5.weight * o0_der;
+                let d_h0 = self.conns.w4.weight * o0_der;
+                let d_h1 = self.conns.w5.weight * o0_der;
+                self.neurons.o0.bias = o0_der;
 
-                let d_w1 = data_slice.x * h1_der;
-                let d_w2 = data_slice.y * h1_der;
-                let d_b1 = h1_der;
+                let d_w1 = data_slice.x * h0_der;
+                let d_w2 = data_slice.y * h0_der;
+                self.neurons.h0.bias = h0_der;
 
-                let d_w3 = data_slice.x * h2_der;
-                let d_w4 = data_slice.y * h2_der;
-                let d_b2 = h2_der;
+                let d_w3 = data_slice.x * h1_der;
+                let d_w4 = data_slice.y * h1_der;
+                self.neurons.h1.bias = h1_der;
 
-                self.conns.w0.weight -= learn_rate * d_mse * d_h1 * d_w1;
-                self.conns.w1.weight -= learn_rate * d_mse * d_h1 * d_w2;
-                self.neurons.h1.bias -= learn_rate * d_mse * d_h1 * d_b1;
+                self.conns.w0.weight -= learn_rate * d_mse * d_h0 * d_w1;
+                self.conns.w1.weight -= learn_rate * d_mse * d_h0 * d_w2;
 
-                self.conns.w2.weight -= learn_rate * d_mse * d_h2 * d_w3;
-                self.conns.w3.weight -= learn_rate * d_mse * d_h2 * d_w4;
-                self.neurons.h2.bias -= learn_rate * d_mse * d_h2 * d_b2;
+                self.conns.w2.weight -= learn_rate * d_mse * d_h1 * d_w3;
+                self.conns.w3.weight -= learn_rate * d_mse * d_h1 * d_w4;
 
                 self.conns.w4.weight -= learn_rate * d_mse * d_w5;
                 self.conns.w5.weight -= learn_rate * d_mse * d_w6;
-                self.neurons.o0.bias -= learn_rate * d_mse * d_b3;
             }
         }
         let elapsed = start.elapsed();
